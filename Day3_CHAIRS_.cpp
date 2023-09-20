c5a0d5cbe2087f6db8effae09c59614b7be3de1f
Anusha <138220216+Anushaa700@users.noreply.github.com>
2023-09-20 19:53:16 +0530

    Add files via upload

---
+++ Day3_CHAIRS_.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T,X,Y;
+	cin>>T;
+	
+	while (T--) {
+	    cin>>X>>Y;
+	    if (Y >= X) {
+	        cout<<0<<endl;
+	    }
+	    else{
+	        cout<<X - Y<<endl;
+	    }
+	}
+	return 0;
+}
